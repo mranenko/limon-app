e8c6886e679eaefebbcf07349626af2a92f13da3
Marina Ranenko <mranenko@appliedmgt.com>
2024-04-24 10:13:33 -0600

    payment screen; input styles; refactor

--- src/screens/Payment.tsx
+++ src/screens/Payment.tsx
@@ -5,6 +5,7 @@ import {
 
 import {
   Button,
+  Card,
 } from '../components'
 import {
   Footer,
@@ -19,7 +20,27 @@ const Payment = () => {
       <Header />
       <Main>
         <section className='flex-column gap-1'>
-          <h2 className='subtitle'>Payment Details</h2>
+          <h2 className='subtitle'>Payment</h2>
+          <Card>
+            <p className='input-group'>
+              <label htmlFor='card-number'>Card number</label>
+              <input className='text' id='card-number' placeholder='**** **** **** ****' type='text' />
+            </p>
+            <div className='flex-row gap-1'>
+              <p className='input-group'>
+                <label htmlFor='card-number'>Expiry</label>
+                <input className='text' id='card-number' placeholder='MM / YY' type='text' />
+              </p>
+              <p className='input-group'>
+                <label htmlFor='card-number'>CVN</label>
+                <input className='text' id='card-number' placeholder='***' type='text' />
+              </p>
+            </div>
+            <p className='input-group'>
+              <label htmlFor='card-number'>Name on card</label>
+              <input className='text' id='card-number' type='text' />
+            </p>
+          </Card>
         </section>
       </Main>
       <Footer>
